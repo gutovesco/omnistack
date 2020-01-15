83f4d6bdce9a4c42233328b7bd83d5bc35e2d3ad
gutovesco <guto.vesco@hotmail.com>
2020-01-15 15:08:51 -0300

    up

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-
+const DevControler = require('./controllers/DevControler');
+const SearchControler = require('./controllers/SearchControler');
 const routes = Router();
 
-routes.get('/', ( request, response) => {
-    console.log(request.body)
-    return response.json({ message: 'Hello OmniStack'});
-});
+routes.get('/devs', DevControler.index);
+routes.post('/devs', DevControler.store);
+
+routes.get('/search', SearchControler.index);
 
 module.exports = routes;
